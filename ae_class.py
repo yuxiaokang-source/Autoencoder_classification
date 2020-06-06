c14cae19735a9550cb910cc3b7b24380bb58f27e
yuxiaokang-source <59059267+yuxiaokang-source@users.noreply.github.com>
2020-06-06 20:06:58 +0800

    Add files via upload

--- ae_class.py
+++ ae_class.py
@@ -21,3 +21,53 @@ testLoader=torch.utils.data.DataLoader(datasets.FashionMNIST('./fmnist/',train=F
                                                              transform=transforms.Compose(
                                                                  [transforms.ToTensor()])),batch_size
                                        =1024,shuffle=True,num_workers=1)
+
+
+import torch
+print(123)
+a=torch.rand(3,3)
+print(a) 
+
+import os
+import struct
+import torch
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import torchvision
+from torchvision import datasets
+
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import copy
+
+
+class autoencoder(nn.Module):
+    def __init__(self):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(28*28, 100),
+            nn.ReLU(),
+            nn.Linear(100, 100),
+            nn.ReLU())
+        self.decoder = nn.Sequential(
+            nn.Linear(100, 100),
+            nn.ReLU(),
+            nn.Linear(100, 28*28),
+            nn.ReLU())
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+net = autoencoder()
+print(net)
+use_gpu = torch.cuda.is_available()
+if use_gpu:
+    net = net.double().cuda()
+else:
+    net = net.double()
+init_weights = copy.deepcopy(net.encoder[0].weight.data)
